af2401a6f62c5899f02df9c84c5513e0960b69dc
Timo <66961498+nuokey@users.noreply.github.com>
2021-03-30 23:52:07 +0300

    Update

--- main.py
+++ main.py
@@ -21,6 +21,13 @@ def save_file():
 	global img, way
 	img.save(way)
 
+def rotate_image():
+	global panel, img, img_panel, way
+	img = img.rotate(int(rotate_entry.get()))
+	img_panel = ImageTk.PhotoImage(img)
+	panel = Label(root, image = img_panel)
+	panel.place(x = 60, y = 0)
+
 root = Tk()
 
 open_button = Button(root, text='Open', command=open_file)
@@ -28,5 +35,11 @@ open_button.place(x = 10, y = 10, width = 40)
 
 save_button = Button(root, text='Save', command=save_file)
 save_button.place(x = 10, y = 40, width = 40)
+
+rotate_button = Button(root, text='Rotate', command=rotate_image)
+rotate_button.place(x = 10, y = 70, width = 40)
+
+rotate_entry = Entry(root)
+rotate_entry.place(x = 10, y = 100, width = 40)
 
 root.mainloop()
